617bd0c006157e1edd610c599e1c53d66672bf17
Taire Stephen <tairestephen@gmail.com>
2022-04-19 20:07:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>test site</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background-color: darkred; color: white;">
+<span class="large">we love God</span>
+
+</body>
+</html>
